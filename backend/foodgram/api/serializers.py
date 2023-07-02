f7fea23ae175de2413e618bc2408b5a704c37b5a
pollot <qwe@gmail.com>
2023-07-02 15:56:15 +0000

    fix validate

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -251,7 +251,7 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
 
     def validate_ingredients(self, data):
         ingredients_list = []
-        ingredients_in_recipe = data.get('IngredientsInRecipe')
+        ingredients_in_recipe = data['IngredientsInRecipe']
         for ingredient in ingredients_in_recipe:
             ingredients_list.append(ingredient['ingredient']['id'])
         if len(ingredients_list) > len(set(ingredients_list)):
